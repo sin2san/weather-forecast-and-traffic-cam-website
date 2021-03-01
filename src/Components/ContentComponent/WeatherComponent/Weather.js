28d8f6b4573b1e94e39ad6306b7ca819980de9bd
Sinthusan <56731626+sin2san@users.noreply.github.com>
2021-03-01 12:28:41 +0800

    Error handling & Styles updated

--- src/Components/ContentComponent/WeatherComponent/Weather.js
+++ src/Components/ContentComponent/WeatherComponent/Weather.js
@@ -4,6 +4,35 @@ import './Weather.css';
 
 class Weather extends Component {
     render() {
+        let weatherIcon = 'wi wi-night-clear';
+        let weatherColor = 'desc-night';
+        if (this.props.location !== null && this.props.location.forecast) {
+            switch (this.props.location.forecast) {
+                case "Fair (Night)":
+                    weatherIcon = 'wi wi-night-clear';
+                    weatherColor = 'desc-night';
+                    break;
+                case "Fair (Day)":
+                    weatherIcon = 'wi wi-day-sunny';
+                    weatherColor = 'desc-day';
+                    break;
+                case "Windy":
+                    weatherIcon = 'wi wi-cloudy-windy';
+                    weatherColor = 'desc-windy';
+                    break;
+                case "Partly Cloudy (Day)":
+                    weatherIcon = 'wi wi-day-cloudy';
+                    weatherColor = 'desc-day-cloudy';
+                    break;
+                case "Fair & Warm":
+                    weatherIcon = 'wi wi-day-sunny-overcast';
+                    weatherColor = 'desc-fair-warm';
+                    break;
+                default:
+                    weatherIcon = 'wi wi-day-sunny';
+                    weatherColor = 'desc-day';
+            }
+        }
         return (
             <Auxiliary>
                 <div className="card">
@@ -11,16 +40,16 @@ class Weather extends Component {
                         <div className="row text-center">
                             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                                 <div className="weather-icon">
-                                    <i className="wi wi-night-clear"></i>
+                                    <i className={weatherIcon}></i>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="row">
+                    <div className="row text-center">
                         <div className="col-md-12">
-                            <div className="desc text-center">
-                                <span>{this.props.location == null ? '' : this.props.location.forecast}</span>
-                                <span>{this.props.location == null ? '' : this.props.location.name}</span>
+                            <div id="desc" className={weatherColor}>
+                                <span>{this.props.location == null ? 'Forecast not found' : this.props.location.forecast}</span>
+                                <span>{this.props.location == null ? 'Area name not found' : this.props.location.name}</span>
                             </div>
                         </div>
                     </div>
